314d231a9f9aece68e0aabe02dfdf6a2bf0f82ec
the-beef-calculator <the-beef-calculator@users.noreply.github.com>
2024-09-07 01:13:28 +0000

    deploy: c3163b668087f8d4c2150ea9e5c55865468d822b

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-80e6fa26d1fd3f7cf5bc.js"
+    "url": "webpack-runtime-17633859b412c7d4b9f1.js"
   },
   {
     "url": "framework-6aadf18978d3da127914.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "2fd455c49ab878944e523a1789a1a662"
+    "revision": "ea1c82d830a93b163bf434d57a5b36df"
   },
   {
     "url": "manifest.webmanifest",
